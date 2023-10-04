b037e08702c89c65bf1da483dc541a1b2d256f34
calvindo95 <calvindo95@gmail.com>
2023-10-04 18:48:37 -0500

    Changed logic to only use absolute path

--- source/IOJson.cpp
+++ source/IOJson.cpp
@@ -14,24 +14,5 @@ std::string IOJson::validate_dir(std::string dir){
         std::filesystem::create_directory(dir);
     }
 
-    // Check if dir contains './' or '/' in first 1-2 chars --> remove those
-    if(dir.substr(0,2) == "./"){
-        dir = dir.substr(2,dir.size());
-    }
-    if(dir.substr(0,1) == "/"){
-        dir = dir.substr(1,dir.size());
-    }
-
-    // Get absolute dir
-    std::string abs_dir = std::filesystem::absolute(dir);
-
-    // Check if last char of dir contains forward slash
-    if(abs_dir.back() != '/'){
-        abs_dir + '/';
-        std::cout << "Appending '/' to " << dir << " --> " << abs_dir << std::endl;     // need to log this later
-        
-        return abs_dir;
-    }
-
-    return abs_dir;
+    return dir;
 }
